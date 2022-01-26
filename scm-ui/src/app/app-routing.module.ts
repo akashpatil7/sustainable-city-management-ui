fae110fceaf3e7e3c9ecac7062dc82edd5ab9de8
Ciara Gilsenan <ciaragil98@gmail.com>
2022-01-26 15:26:02 +0000

    add routing

--- scm-ui/src/app/app-routing.module.ts
+++ scm-ui/src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserServiceComponentComponent } from './user-service-component/user-service-component.component';
 
 const routes: Routes = [
-  { path: 'login', component: UserServiceComponentComponent }
+  { path: 'login', component: UserServiceComponentComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full'}
 ];
 
 @NgModule({
