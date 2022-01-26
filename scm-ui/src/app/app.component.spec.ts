d49b0029d7f6e1455393dfa1d0972b2ee5c82342
Sophie Crowley <crowleso@tcd.ie>
2022-01-26 15:47:44 +0000

    Making Initial Tests Pass

--- scm-ui/src/app/app.component.spec.ts
+++ scm-ui/src/app/app.component.spec.ts
@@ -20,16 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'scm-ui'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('scm-ui');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('scm-ui app is running!');
+    expect(compiled.querySelector('.toolbar span')?.textContent).toContain('Welcome to Sustainable City Management');
   });
 });
--- scm-ui/src/app/user-service-component/user-service-component.component.spec.ts
+++ scm-ui/src/app/user-service-component/user-service-component.component.spec.ts
@@ -2,12 +2,17 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserServiceComponentComponent } from './user-service-component.component';
 
+import { FormsModule } from '@angular/forms';
+
 describe('UserServiceComponentComponent', () => {
   let component: UserServiceComponentComponent;
   let fixture: ComponentFixture<UserServiceComponentComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        FormsModule
+      ],
       declarations: [ UserServiceComponentComponent ]
     })
     .compileComponents();
