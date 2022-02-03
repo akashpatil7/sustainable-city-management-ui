d072a2e9e07ec4faf68335c1f3d1ae48bc2df416
Maddie <maddie.comtois@gmail.com>
2022-02-03 16:56:33 +0000

    finish bike hourly averages

--- scm-ui/src/app/trends.service.ts
+++ scm-ui/src/app/trends.service.ts
@@ -9,14 +9,9 @@ export class TrendsService {
 
   constructor(private http: HttpClient) { }
   
-  // get trends from backend as JSON object
-  getTrendsFilterByDate() : Observable<any> {
-    return this.http.post("http://localhost:7001/trends/getTrendsData", {"startDate": "2021-01-01","endDate": "2021-02-02","dataIndicator": "bikes"});
-  }
-  
-  getTrends():Observable<any> {
-    return this.http.get("http://localhost:8050/getCurrentHourAverages");
-    //return this.http.get("http://localhost:7001/trends/getTrendsData");
+  // get hourly bike averages
+  getHourlyAverage():Observable<any> {
+    return this.http.get("http://localhost:8050/getHourlyAverageForAllStation");
   }
   
 }
