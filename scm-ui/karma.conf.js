7d0b178e3fd8e1c2838cb342d5f46c78369d91ef
Sophie Crowley <crowleso@tcd.ie>
2022-02-11 18:08:49 +0000

    Github Action ng test failing issues
    Update karma.conf.js
    
    Update node.yml
    
    Update node.yml
    
    Attempting to fix karma
    
    Update node.yml
    
    Update 2.0
    
    Revert "Update node.yml"
    
    This reverts commit 8b300f9e1ad9dff92337db541e42e710f7e0e902.
    
    Trying different launcher
    
    Update node.yml
    
    Attempting ChromeHeadless

--- scm-ui/karma.conf.js
+++ scm-ui/karma.conf.js
@@ -37,14 +37,22 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless'],
+    browserDisconnectTimeout: 10000,
+    browserDisconnectTolerance: 3,
+    browserNoActivityTimeout: 60000,
+    flags: [
+      '--disable-web-security',
+      '--disable-gpu',
+      '--no-sandbox'
+    ],
     singleRun: false,
     restartOnFileChange: true,
     customLaunchers: {
-      ChromeHeadlessCustom: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu']
+      Chrome_no_sandbox: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
       }
-    }
+    },
   });
 };
