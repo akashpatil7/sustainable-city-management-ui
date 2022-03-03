30da3caee9a9e2eb2dde77f4abedc99b590ab957
Maddie <maddie.comtois@gmail.com>
2022-03-03 18:06:57 +0000

    Update search-filter.pipe.spec.ts

--- src/app/pipes/search-filter.pipe.spec.ts
+++ src/app/pipes/search-filter.pipe.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { Observable, Observer } from 'rxjs';
 import { RealTimeDataService } from '../services/real-time-data-service.service';
-import { AqiSearchFilterPipe } from './aqi-search-filter.pipe';
 import { SearchFilterPipe } from './search-filter.pipe';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -11,7 +10,7 @@ describe('SearchFilterPipe', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AqiSearchFilterPipe, SearchFilterPipe ],
+      declarations: [ SearchFilterPipe ],
       imports: [
         HttpClientModule,
         RouterTestingModule,
@@ -22,26 +21,6 @@ describe('SearchFilterPipe', () => {
 
   it('create an instance', () => {
     pipe = new SearchFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
-
-describe('AqiSearchFilterPipe', () => {
-  let pipe: AqiSearchFilterPipe;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AqiSearchFilterPipe, SearchFilterPipe ],
-      imports: [
-        HttpClientModule,
-        RouterTestingModule,
-      ],
-    })
-    .compileComponents();
-  });
-
-  it('create an instance', () => {
-    pipe = new AqiSearchFilterPipe();
     expect(pipe).toBeTruthy();
   });
 });
